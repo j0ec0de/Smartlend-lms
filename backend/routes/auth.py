b65cd45b7a6068c5a16161f297533a7f39927909
j0ec0de <joelanto386@gmail.com>
2025-12-09 22:27:25 +0530

    implemented normal auth and changed app to extensions import for db

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -9,6 +9,11 @@ auth_bp = Blueprint("auth", __name__)
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.json
+
+    existing_user = User.query.filter_by(email=data["email"]).first()
+    if(existing_user):
+        return jsonify({"message": "User already exists"}), 409
+
     hashed_pw = bcrypt.generate_password_hash(data["password"]).decode('utf-8')
 
     user = User(name=data["name"], email=data["email"], password=hashed_pw)
@@ -16,3 +21,22 @@ def register():
     db.session.commit()
 
     return jsonify({"message": "User registered successfully"}), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.json
+
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if user and bcrypt.check_password_hash(user.password, data["password"]):
+        return jsonify({
+            "message": "User Logged in successfully",
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email
+            }
+        }), 200
+
+
+    return jsonify({"message": "Invalid email or password"}), 401
